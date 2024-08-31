b007183c7c10df66647f340e7123be035fffeb89
Xiomara Canizales <53951477+XiomaraCanizales@users.noreply.github.com>
2024-08-30 22:59:24 -0600

    Updating CSS to my liking

--- freecodecamp/10-TODO-app/script.js
+++ freecodecamp/10-TODO-app/script.js
@@ -56,12 +56,13 @@ const updateTaskContainer = () => {
         ({id, title, date, description}) => {
         tasksContainer.innerHTML += 
         `
-            <div class="task" id="${id}"></div>
-            <p><strong>Title:</strong>${title}</p>
-            <p><strong>Date:</strong>${date}</p>
-            <p><strong>Description:</strong>${description}</p>
-            <button type="button" class="btn" onclick="editTask(this)">Edit</button>
-            <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+            <div class="task" id="${id}">
+                <p><strong>Title:</strong>${title}</p>
+                <p><strong>Date:</strong>${date}</p>
+                <p><strong>Description:</strong>${description}</p>
+                <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+                <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+            </div>
         `
     })
 }
